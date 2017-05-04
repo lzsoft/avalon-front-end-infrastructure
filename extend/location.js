2121679bf46224fcabd15f6bf9dddc2fdd2bede8
JingAn Chen <lzsoft.cja@gmail.com>
2017-05-04 11:07:19 -0400

    Update the function name.

--- extend/location.js
+++ extend/location.js
@@ -3,7 +3,7 @@ window.location.staticAssign = function(val) {
     window.history.pushState(null, null, val);
     window.dispatchEvent(new Event("popstate"));
 };
-window.location.pathLastSection = function() {
+window.location.getPathLastSegment = function() {
     let path = window.location.path;
     return window.location.pathname.split('/').pop();
 };
