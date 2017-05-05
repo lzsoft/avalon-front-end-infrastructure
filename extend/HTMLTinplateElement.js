eedc7271cb8f46ca2b3ba1c4644e26242ad84cfe
JingAn Chen <lzsoft.cja@gmail.com>
2017-05-05 18:22:40 -0400

    Content property is more appropriate to <template> element because we can use this to detect whether browser supports <template>.

--- extend/HTMLTinplateElement.js
+++ extend/HTMLTinplateElement.js
@@ -3,6 +3,6 @@ window.HTMLTinplateElement = class extends HTMLElement {
     constructor() {
         super();
         // In the future, optimize the template getting process and implement shadowRoot here
-        this.innerHTML = document.querySelector('link[href*="' + this.tagName.toLowerCase() + '.html"]').import.querySelector("template").innerHTML;
+        this.innerHTML = document.querySelector('link[href*="' + this.tagName.toLowerCase() + '.html"]').import.querySelector("template").content;
     }
 }
