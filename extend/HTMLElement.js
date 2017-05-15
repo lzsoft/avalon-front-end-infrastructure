2155207f6ab0dbb0e7d16dd80727e06ca97d4b5c
JingAn Chen <lzsoft.cja@gmail.com>
2017-05-15 12:21:09 -0400

    Because W3C custom elements constructor spec, we can't set innerHTML in constructor.

---
+++ extend/HTMLElement.js
@@ -0,0 +1,4 @@
+'use strict';
+HTMLElement.prototype.importCustomElementTemplateFromHTMLImport = function() {
+    this.innerHTML = document.querySelector('link[href*="' + this.tagName.toLowerCase() + '.html"]').import.querySelector("template").innerHTML;
+}
--- extend/HTMLTinplateElement.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-window.HTMLTinplateElement = class extends HTMLElement {
-    constructor() {
-        super();
-        // In the future, optimize the template getting process and implement shadowRoot here
-        this.innerHTML = document.querySelector('link[href*="' + this.tagName.toLowerCase() + '.html"]').import.querySelector("template").innerHTML;
-    }
-}
