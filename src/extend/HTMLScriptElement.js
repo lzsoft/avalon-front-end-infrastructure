ab116bcbe3fc44647f8132d0561011d09de958b2
JingAn Chen <lzsoft.cja@gmail.com>
2017-07-31 16:58:46 -0400

    Correct the use of promise.

--- src/extend/HTMLScriptElement.js
+++ src/extend/HTMLScriptElement.js
@@ -1,6 +1,6 @@
 window.HTMLScriptElement.prototype.loadSameNameHTML = function(callback) {
     fetch(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html').then(function(response) {
-        response.text(function(result) {
+        response.text().then(function(result) {
             callback(result);
         }, function(error) {
             console.error(error);
