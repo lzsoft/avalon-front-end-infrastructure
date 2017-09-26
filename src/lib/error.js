c9de16066f0d8a36758528e833ec3a7c96c8277f
JingAn Chen <lzsoft.cja@gmail.com>
2017-09-26 16:40:45 -0400

    Should use Set.

--- src/lib/error.js
+++ src/lib/error.js
@@ -1,13 +1,13 @@
 window.tingting.error = {};
-window.tingting.error.e = {};
+window.tingting.error.e = new Set();
 window.tingting.error.register = function(element) {
-    window.tingting.error.e[element] = 1;
+    window.tingting.error.e.add(element);
 };
 window.tingting.error.unregister = function(element) {
-    delete window.tingting.error.e[element];
+    window.tingting.error.e.delete(element);
 };
 window.tingting.error.trigger = function(source, text) {
-    for (let i in window.tingting.error.e) {
+    for (let i of window.tingting.error.e) {
         i.dispatchEvent(new CustomEvent("ERROR", { detail: { source, text } }));
     }
 };
