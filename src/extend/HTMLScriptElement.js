f53d0c588ff04c3b599aded56cee44fed2bf551f
JingAn Chen <lzsoft.cja@gmail.com>
2018-01-12 15:19:39 -0500

    Adding a Electron/Nodejs apadation for loadSameNameHTML.
    Use Nodejs native fs library instead of fetch while it's running under
    Electron. Because Fetch API can not and will never handle file://
    protocol.

--- src/extend/HTMLScriptElement.js
+++ src/extend/HTMLScriptElement.js
@@ -1,11 +1,22 @@
-window.HTMLScriptElement.prototype.loadSameNameHTML = function(callback) {
-    fetch(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html').then(function(response) {
-        response.text().then(function(result) {
-            callback(result);
-        }, function(error) {
-            console.error(error);
+const fs = require ? require('fs') : null;
+window.HTMLScriptElement.prototype.loadSameNameHTML = function (callback) {
+    if (fs) {
+        // means we're in Electron/Nodejs
+        fs.readFile(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html', function read(err, data) {
+            if (err) {
+                throw err;
+            }
+            callback(data);
         });
-    }, function(error) {
-        console.error(error);
-    });
+    } else {
+        fetch(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html').then(function (response) {
+            response.text().then(function (result) {
+                callback(result);
+            }, function (err) {
+                throw err;
+            });
+        }, function (err) {
+            throw err;
+        });
+    }
 };
