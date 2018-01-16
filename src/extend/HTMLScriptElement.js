f86338e90fe847b68fda9978be20b48e5b47eb06
JingAn Chen <lzsoft.cja@gmail.com>
2018-01-16 12:09:58 -0500

    Use URI module to load the file.
    This should be a more guaranteed way while loading file via file://
    protocol.

--- src/extend/HTMLScriptElement.js
+++ src/extend/HTMLScriptElement.js
@@ -1,22 +1,27 @@
-const fs = require ? require('fs') : null;
-window.HTMLScriptElement.prototype.loadSameNameHTML = function (callback) {
-    if (fs) {
-        // means we're in Electron/Nodejs
-        fs.readFile(decodeURI(document.currentScript.src.substr(8, document.currentScript.src.length - 11) + ".html"), function read(err, data) {
-            if (err) {
-                throw err;
-            }
-            callback(data);
-        });
-    } else {
-        fetch(decodeURI(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html')).then(function (response) {
-            response.text().then(function (result) {
-                callback(result);
+{
+    const fs = require ? require('fs') : null;
+    const URL = require ? require('url').URL : null;
+    window.HTMLScriptElement.prototype.loadSameNameHTML = function (callback) {
+        let u = document.currentScript.src.substr(0, document.currentScript.src.lastIndexOf('.')) + '.html';
+        if (fs) {
+            // means we're in Electron/Nodejs
+            let u = new URL(decodeURI(u));
+            fs.readFile(u, function read(err, data) {
+                if (err) {
+                    throw err;
+                }
+                callback(data);
+            });
+        } else {
+            fetch(u).then(function (response) {
+                response.text().then(function (result) {
+                    callback(result);
+                }, function (err) {
+                    throw err;
+                });
             }, function (err) {
                 throw err;
             });
-        }, function (err) {
-            throw err;
-        });
-    }
-};
+        }
+    };
+}
