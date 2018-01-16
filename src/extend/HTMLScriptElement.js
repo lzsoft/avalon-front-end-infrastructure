2aead5784b521a04085f46817b80c25f15140c0b
JingAn Chen <lzsoft.cja@gmail.com>
2018-01-16 11:29:17 -0500

    Adding a decodeURI to the currentScript same name HTML loader.
    When running under Electron, if the path contains a space or other
    special character, in URL structure it'll automatically be encoded. We
    need to decode it again so it can become OS level filepath again.

--- src/extend/HTMLScriptElement.js
+++ src/extend/HTMLScriptElement.js
@@ -2,14 +2,14 @@ const fs = require ? require('fs') : null;
 window.HTMLScriptElement.prototype.loadSameNameHTML = function (callback) {
     if (fs) {
         // means we're in Electron/Nodejs
-        fs.readFile(document.currentScript.src.substr(8, document.currentScript.src.length - 11) + ".html", function read(err, data) {
+        fs.readFile(decodeURI(document.currentScript.src.substr(8, document.currentScript.src.length - 11) + ".html"), function read(err, data) {
             if (err) {
                 throw err;
             }
             callback(data);
         });
     } else {
-        fetch(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html').then(function (response) {
+        fetch(decodeURI(document.currentScript.src.substr(0, document.currentScript.src.length - 3) + '.html')).then(function (response) {
             response.text().then(function (result) {
                 callback(result);
             }, function (err) {
