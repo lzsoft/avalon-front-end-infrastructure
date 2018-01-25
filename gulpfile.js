76c64495c8bff54a16fcb8f4ff385f325248d00d
JingAn Chen <lzsoft.cja@gmail.com>
2018-01-25 16:04:39 -0500

    Remove the clean part of the gulp script.
    We don't want to accidentally remove the src folder while developing on
    local.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
 const gulp = require('gulp');
 const minifyJS = require('gulp-minify');
-const del = require('del');
 const concat = require('gulp-concat');
-gulp.task('js', function () {
+gulp.task('build', function () {
     gulp.src('./src/**/*.js').pipe(minifyJS({
         ext: {
             min: '.js'
@@ -10,9 +9,3 @@ gulp.task('js', function () {
         noSource: true
     })).pipe(concat('avalon-front-end-infrastructure.js')).pipe(gulp.dest('./min/'));
 });
-gulp.task('clean', function () {
-    del('./src/', {
-        force: true
-    });
-});
-gulp.task('build', ['js', 'clean']);
