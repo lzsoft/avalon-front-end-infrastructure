498e58146040740fc5ff10ed723a7e472b64d288
JingAn Chen <lzsoft.cja@gmail.com>
2018-02-13 16:38:14 -0500

    Add a clean script after the install.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const gulp = require('gulp');
 const minifyJS = require('gulp-minify');
 const concat = require('gulp-concat');
+const del = require('del');
 gulp.task('build', function () {
     gulp.src('./src/**/*.js').pipe(minifyJS({
         ext: {
@@ -8,4 +9,9 @@ gulp.task('build', function () {
         },
         noSource: true
     })).pipe(concat('avalon-front-end-infrastructure.js')).pipe(gulp.dest('./min/'));
+});
+gulp.task('clean', function () {
+    del('./src/', {
+        force: true
+    });
 });
